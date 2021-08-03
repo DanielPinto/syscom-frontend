550413789cf60f4c2cf08c77b5fd8432e39cf96a
DanielPinto <danielsp.pinto@gmail.com>
2021-08-03 17:31:03 -0300

    foi implementado a conexão com a api para o login recebendo um token

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,23 +1,67 @@
-import { React } from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 import './style.css';
 
 const Login = () => {
+
+    const initialValues = {
+        email: '',
+        password: ''
+    };
+
+    const [form, setForm] = useState(initialValues);
+
+    const onChange = ev => {
+        let name = ev.target.name;
+        let value = ev.target.value;
+
+        setForm(
+            {
+                ...form,
+                [name]: value
+            }
+        );
+    }
+
+    const onSubmit = ev => {
+        ev.preventDefault();
+
+        const headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          }
+
+          console.log(form);
+          
+          axios.post("http://localhost:8000/api/login", 
+          form,
+          {
+            headers: headers
+          })
+            .then((response) => {
+              console.log(response.data);
+            })
+            .catch((error) => {
+                console.log("ERRO_APP:" + error);
+            });
+    }
+
     return (
         <div className="container">
-            <form className="form-signin">
+            <form className="form-signin" onSubmit={onSubmit}>
 
                 <div>
                     <h2>LOGIN</h2>
                 </div>
 
                 <div className="form-label-group">
-                    <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
+                    <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus name="email" onChange={onChange} />
                     <label for="inputEmail">Email address</label>
                 </div>
 
                 <div className="form-label-group">
-                    <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
+                    <input type="password" id="inputPassword" className="form-control" placeholder="Password" required name="password" onChange={onChange} />
                     <label for="inputPassword">Password</label>
                 </div>
 
